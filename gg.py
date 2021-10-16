5b1083362bbdc935239a34f01d17bc0150b5fb5c
Lamppy <90641456+Lamppy@users.noreply.github.com>
2021-10-16 20:37:51 +0300

    taylor

---
+++ gg.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import math
+import numpy
+x= numpy.arange(-10,10,0.1)
+plt.plot(x,x*x)
+plt.show()
--- taylor_series.py
+++ taylor_series.py
@@ -1,20 +1,29 @@
+"""Добавление некоторых арифметических функций и возможности построения графика"""
 import math
-accuracy = 20
+import numpy as np
+import matplotlib.pyplot as plt 
 
-def my_cos(x):
 
-    x_degree = 1
-    multiplier = 1
+
+ACCURACY=20
+
+def my_cos(*x_arg):
+    """Функция разложения косинуса по Тейлору"""
+    x_degree=1
+    multiplier=1
     partial_sum = 1
+    for temp_number in range(2,ACCURACY):      
+        x_degree*=x_arg**2  
+        multiplier*=((-1))/(math.factorial((2*temp_number-2)))
+        partial_sum+=x_degree*multiplier  
+        return partial_sum
+if __name__ == '__mane__':    
 
-    for n in range(2, accuracy):
-
-        x_degree *= x**2  
-        multiplier *= ((-1)**(n-1))/(math.factorial((2*n-2)))
-        partial_sum += x_degree * multiplier  
-
-    return partial_sum
-
-if __name__ == "__main__":
-    x = float(input())
-    print(my_cos(x))
+    vs = np.vectorize(my_cos)
+    print(my_cos, vs)
+    x = np.arange(-10,10,0.1)
+    angles = np.r_[-16.25:16.25:0.01]
+    plt.plot(x, my_cos(x))
+    plt.plot(angles, np.cos(angles))
+    plt.plot(angles, vs(angles))
+    plt.show()
